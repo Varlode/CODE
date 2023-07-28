db35ca1ea0700708051facf949e6e7e4f5376253
Varlode <tyrrtyrr06@gmail.com>
2023-07-28 22:05:41 +0700

    Update code full HSG 2021 Vinh Long

--- CodeCpp/HSG 2021/SONNEN.cpp
+++ CodeCpp/HSG 2021/SONNEN.cpp
@@ -5,26 +5,17 @@ int n;
 string s1, s2, sn;
 
 string op(string a, string b) {
-    string ans;
     int mem = 0;
-    while (a.size() && b.size()) {
-        ans.push_back(char((a.back()+b.back()-2*'0'+mem)%10+'0'));
-        mem = (a.back()+b.back()-2*'0'+mem)/10;
-        a.pop_back();
-        b.pop_back();
+    while (a.size() < b.size()) a = '0' + a;
+    while (a.size() > b.size()) b = '0' + b;
+    string ans = a;
+    int i = a.size() - 1;
+    while (i >= 0) {
+        ans[i] = char((a[i]+b[i]-2*'0'+mem)%10+'0');
+        mem = (a[i]+b[i]-2*'0'+mem)/10;
+        i--;
     }
-    while (a.size()) {
-        ans.push_back(char((a.back()-'0'+mem)%10+'0'));
-        mem = (a.back()-'0'+mem)/10;
-        a.pop_back();
-    }
-    while (b.size()) {
-        ans.push_back(char((b.back()-'0'+mem)%10+'0'));
-        mem = (b.back()-'0'+mem)/10;
-        b.pop_back();
-    }
-    if (mem == 1) ans.push_back('1');
-    reverse(ans.begin(), ans.end());
+    if (mem == 1) ans = '1' + ans;
     return ans;
 }
 
--- CodeCpp/HSG 2021/nghivan.cpp
+++ CodeCpp/HSG 2021/nghivan.cpp
@@ -1,20 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int o1, o2, o3, o4, n, cnt[4];
+int o1, o2, o3, o4, n, cnt[4], s, b, d;
 
 int check(int f) {
     if (0 <= f && f <= 10) return 1;
-    if (10 <= f && f <= 100) return 2;
+    if (10 < f && f < 100) return 2;
     if (100 <= f && f <= 255) return 3;
 }
 
 int main() {
+    freopen("nghivan.inp", "r", stdin);
+    freopen("nghivan.out", "w", stdout);
     cin >> n;
+    s = b = d = 0;
     while (n--) {
         cnt[1] = 0, cnt[2] = 0, cnt[3] = 0;
         cin >> o1 >> o2 >> o3 >> o4;
         cnt[check(o1)]++; cnt[check(o2)]++; cnt[check(o3)]++; cnt[check(o4)]++;
-        if (cnt[3] >= 1 || cnt[1] == 4) continue;
+        if (cnt[3] >= 2 || cnt[1] == 4) continue;
+        if (cnt[3] == 1 && cnt[1] == 3) continue;
+        if (cnt[3]+cnt[2] >= 2) d++;
+        else b++;
+        s++;
     }
+    cout << s << ' ' << b << ' ' << d;
 }
